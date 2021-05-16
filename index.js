e773504a2961e31099194abb474a5947f381a0f7
HashirIsmail <84286266+HashirIsmail@users.noreply.github.com>
2021-05-16 22:58:29 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+console.log('Me');
+console.log('🤠');
+console.log('Sister');
+console.log('😡');
+console.log('Mother');
+console.log('🥰');
+console.log('Father');
+console.log('😎');
+alert('Thankyou😁');
